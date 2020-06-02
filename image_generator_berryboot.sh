219364382e0e40b60af0875d657e28cdab7797b4
DevDemo <Demon77@web.de>
2020-06-02 10:12:35 +0200

    Update

--- image_generator_berryboot.sh
+++ image_generator_berryboot.sh
@@ -14,20 +14,19 @@ date=$(date +"%d-%b-%Y")
 sleep 1
 clear
 #Some artwork...
-echo "-------------------------------------------------";
-echo "  ___                   ___                      ";
-echo " | _ ) ___ _ _ _ _ _  _/ __| ___ _ ___ _____ _ _ ";
-echo " | _ \/ -_) '_| '_| || \__ \/ -_) '_\ V / -_) '_|";
-echo " |___/\___|_| |_|  \_, |___/\___|_|  \_/\___|_|  ";
-echo " 2019.09.15        |__/                          ";
-echo "-------------------------------------------------";
+echo "----------------------------------------------------------";
+echo " _,_ _,_,  _    __, _    __,_  ,_____,_, _ _, _ _,_, _ _, ";
+echo " |_//_\|   |    |_) |    |_ '\/  | |_ |\ |(_  |/ \|\ |(_  ";
+echo " | \| || , |    |   |    |   /\  | |  | \|, ) |\ /| \|, ) ";
+echo " ~ ~~ ~~~~ ~    ~   ~    ~~~~  ~ ~ ~~~~  ~ ~  ~ ~ ~  ~ ~  ";
+echo "----------------------------------------------------------";
 echo ""
 echo "#### INSTALLING DEPENDENCIES ####"
 echo ""
 
 #OS Menu Selection
 PS3='Please select your OS: '
-options=("Debian/Ubuntu" "CentOS" "Fedora" "ArchLinux" "Skip")
+options=("Skip" "Debian/Ubuntu" "CentOS" "Fedora" "ArchLinux")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -105,7 +104,7 @@ echo ""
 
 #OS Menu Selection
 PS3='Please select the OS: '
-options=("Android" "Kali Linux" "Ubuntu eoan" "Debian 9" "RetroPie Armbian" "LibreELEC (Kodi)" "Exit")
+options=("Android" "Kali Linux" "Ubuntu eoan" "Debian (dosbian_v1.5)" "RetroPie Armbian" "LibreELEC (Kodi)" "Exit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -114,58 +113,23 @@ do
 bash <(wget -qO- https://github.com/D3vD3m0n/Image-Generator-for-Berryboot/blob/master/os-selection/libreelec/libreelec_by_berryserver.sh)
 		break
             ;;
-        "Exit")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
-    case $opt in
         "Kali Linux")
-#Starting Kali Script
 bash <(wget -qO- https://github.com/D3vD3m0n/Image-Generator-for-Berryboot/blob/master/os-selection/kali/Kali_2020_2a_berryboot.sh)
 		break
             ;;
-        "Exit")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
-    case $opt in
         "Android")
-#Starting LibreELEC Script
 bash <(wget -qO- https://github.com/D3vD3m0n/Image-Generator-for-Berryboot/blob/master/os-selection/android/android_by_berryserver.sh)
 		break
             ;;
-        "Exit")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
-    case $opt in
         "Ubuntu eoan")
-#Starting LibreELEC Script
 bash <(wget -qO- https://github.com/D3vD3m0n/Image-Generator-for-Berryboot/blob/master/os-selection/ubuntu/ubuntu_by_berryserver.sh)
 		break
             ;;
-        "Exit")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
-    case $opt in
-        "Debian 9")
-#Starting LibreELEC Script
+        "Debian (dosbian_v1.5)")
 bash <(wget -qO- https://github.com/D3vD3m0n/Image-Generator-for-Berryboot/blob/master/os-selection/debian/debian_by_berryserver.sh)
 		break
             ;;
-        "Exit")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
-     case $opt in
         "RetroPie Armbian")
-#Starting LibreELEC Script
 bash <(wget -qO- https://github.com/D3vD3m0n/Image-Generator-for-Berryboot/blob/master/os-selection/retropie/retropie_by_berryserver.sh)
 		break
             ;;
